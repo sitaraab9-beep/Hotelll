b5bfeba98f3e41e6cd7e6ef5c5ee7de2048964f7
Savita P N <nekarsavita@gmail.com>
2025-10-23 18:54:43 +0530

    Revert to original backend with CORS fix

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,21 @@ const testRoutes = require('./routes/test');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 // Test route
 app.get('/api/test', (req, res) => {
